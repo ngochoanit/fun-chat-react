ec9451f1df296b2308115995468ce282932a56aa
ngochoanit <phamngochoan.it@gmail.com>
2021-10-27 10:23:46 +0700

    start project

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row, Col } from 'antd'
+
+function Login(props) {
+    return (
+        <div>
+            <Row>
+                <Col></Col>
+            </Row>
+        </div>
+    );
+}
+
+export default Login;
